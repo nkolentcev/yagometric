c9c2068c6fd6f9db393208be69fd116291279985
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-01-03 18:16:43 +0300

    clear server package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"handlers"
-	"log"
-	"net/http"
-	"storage"
-)
-
-const endpoint = "127.0.0.1:8080"
-
 func main() {
 
-	memStorage := storage.NewMemStorage()
-	handler := handlers.NewMetricHandler(memStorage)
-	mux := http.NewServeMux()
-	mux.Handle("/update/", handler)
-
-	fmt.Println("Start server")
-	server := &http.Server{
-		Addr:    endpoint,
-		Handler: mux,
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
 }
