d8f1c694f62b348e5ded8c4d5c378ab81cb1a06e
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-01-05 12:03:42 +0300

    add srv and storage

---
+++ cmd/server/handlers/handlers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/nkolentcev/yagometric/cmd/server/storage"
+)
+
+type MyMetricHandler struct {
+	storage storage.Storage
+}
+
+func NewMetricHandler(storage storage.Storage) MyMetricHandler {
+	return MyMetricHandler{storage: storage}
+}
+
+func (mh MyMetricHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(rw, "its not a post method on request", http.StatusBadRequest)
+		return
+	}
+	url := r.URL.Path
+	url = strings.TrimPrefix(url, "/")
+	url = strings.TrimSuffix(url, "/")
+	sl := strings.Split(url, "/")
+
+	if len(sl) != 4 {
+		http.Error(rw, "wrong endpoint schema", http.StatusBadRequest)
+		return
+	}
+
+	metricName := sl[2]
+	metricValue, err := strconv.ParseFloat(sl[3], 64)
+	if err != nil {
+		http.Error(rw, "wrong metric value", http.StatusBadRequest)
+	}
+	mh.storage.AddMetrics(metricName, metricValue)
+
+	rw.Header().Set("Content-Type", "text/plain")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(nil)
+}
