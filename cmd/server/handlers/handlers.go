9642a039ecc9993d0af877a02a23542d12e07173
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-01-06 02:21:05 +0300

    коды ответа по тесту

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -20,8 +20,6 @@ func NewMetricHandler(storage *storage.MemStorage) *MyMetricHandler {
 
 func (mh MyMetricHandler) UpdateMetric(w http.ResponseWriter, r *http.Request) {
 
-	w.WriteHeader(http.StatusOK)
-
 	name := chi.URLParam(r, "name")
 
 	metricType := chi.URLParam(r, "type")
@@ -37,6 +35,7 @@ func (mh MyMetricHandler) UpdateMetric(w http.ResponseWriter, r *http.Request) {
 		log.Println("unable convert string metric")
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 	mh.storage.AddMetric(name, value, metricType)
 
 }
