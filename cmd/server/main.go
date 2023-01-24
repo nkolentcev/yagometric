9378df96c289a79da78dd2d5d8c9f3458b4cde34
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-01-25 02:56:56 +0300

    доработал настройку агента и сервера через переменные окружения со значениями по умолчанию

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/nkolentcev/yagometric/internal/config"
 	"github.com/nkolentcev/yagometric/internal/handlers"
 	"github.com/nkolentcev/yagometric/internal/storage"
 )
@@ -12,14 +13,14 @@ import (
 const endpoint = ":8080"
 
 func main() {
-
+	scfg := config.NewServerCfg()
 	storage := storage.NewMemStorage()
 	handler := handlers.NewMetricHandler(storage)
 	routers := handler.Router()
 	r := chi.NewRouter()
 	r.Mount("/", routers)
 
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(scfg.Address, r)
 	if err != nil {
 		log.Println(err)
 	}
